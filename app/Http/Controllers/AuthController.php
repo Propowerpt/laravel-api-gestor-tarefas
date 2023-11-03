897bd2cda42948c2abe1207a2117ec8b9390abf3
Diogo <lisboadiogo2010@hotmail.com>
2023-11-03 12:01:34 +0000

    criaçao do token no login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,12 +3,27 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    public function login(){
-        return "login teste";
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+ 
+        //NO POSTMAN PREENCHER cOM EMAIL VALIDO E PASSWORD POR ALGUMA RAZAO O POSTMAN NÃO DEVOLVE 
+        //ERRO CASO OS CAMPOS NÃO ESTEJAM VALIDOS
+        if (Auth::attempt($credentials)) {
+            $token = $request->user()->createToken('invoice');
+            return response()->json(['sucesso' => 'Entrada autorizada','token' => $token->plainTextToken], 200);
+        }else{
+            return response()->json(['error' => 'Credenciais erradas'], 400);
+        }
     }
 
     public function logout(){
